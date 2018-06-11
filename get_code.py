e5cc56aedfa64ea7e9e4537a26f04894c95264a7
xu.mingxun <xu.mingxun@puscene.com>
2018-06-11 19:10:58 +0800

    add

---
+++ get_code.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+import ssl
+from urllib.request import urlretrieve
+import urllib
+import os
+import shutil
+
+while True:
+    url='https://passport.99fund.com/cif/login/loginVerifyCode.htm?time=1528702116568'
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    data = urllib.request.urlopen(req).read()
+
+    with open('./9427.jpg','wb') as f:
+        f.write(data)
+
+    str=""
+    while len(str)!=4:
+        str = input("code=")
+        if str=='':
+            break
+
+    if len(str)==4:
+        shutil.move('./9427.jpg','./code/{}.jpg'.format(str))
---
+++ model.py
@@ -0,0 +1,9 @@
+class Model(object):
+    def __init__(self):
+        pass
+    def build(self):
+        pass
+    def train(self):
+        pass
+    def predict(self,datas):
+        pass
